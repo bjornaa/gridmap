ef0569fe387c7b02cf38482b0ff5b71ed14294f1
Bjørn Ådlandsvik <bjorn@imr.no>
2012-06-29 15:23:15 +0200

    updated project_basemap.py

--- examples/project_basemap.py
+++ examples/project_basemap.py
@@ -1,93 +1,78 @@
-#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Use basemap for a polar stereographic projection
-# without gridmap
+"""Use basemap for a polar stereographic projection"""
 
-# ----------------------------------
-# project_basemap.py
-#
+# This example uses gridmap only for comparison
+
+# --------------------------------
 # Bjørn Ådlandsvik <bjorn@imr.no>
 # Institute of Marine Research
-# 2012-06-04
-# ----------------------------------
-
+# 2012-06-29
+# --------------------------------
 
 from mpl_toolkits.basemap import Basemap
-
-# -------------------
-# User settings
-# -------------------
+import gridmap
 
 # Grid parameters
 xp    = 418.25        # x grid coordinate of north pole
 yp    = 257.25        # y grid coordinate of north pole
-dx    = 10000         # grid resolution (at lat_c)       [m]
-ylon  = 58.0          # angle of y-axis                  [°N]
+dx    = 10000         # grid resolution (at lat_ts)      [m]
+ylon  = 58.0          # angle of y-axis                  [deg]
 
-lon_ts = 60.0         # Latitude of true scale           [°N]
+# Position to project
+lon, lat = 2, 66      # Station M
 
-lonlat_file = "lonlat.dat"  # File with longitude-latitude data
-
-
-# -----------------
-# Read the data 
-# -----------------
-
-f = open(lonlat_file)
-
-lonlats = []
-for line in f:
-    w = line.split()
-    lonlats.append((float(w[0]), float(w[1])))
-    
-f.close()
-
-# --------------
+# ------------------------
 # Sphere case
-# --------------
+# ------------------------
 
-print " --- Sphere --- "
+print "\n --- Sphere --- \n"
 
-# Setup the Basemap projection
-# urcrnlon, urcrnrlat is arbitrary
-p = Basemap(projection='stere', rsphere=6371000,
+# Set up projection in basemap, origin at North Pole
+# with dummy values for urcrnrlon and urcrnrlat
+bmap = Basemap(projection='stere', rsphere=6371000,
+               llcrnrlon = ylon, llcrnrlat = 90.0,
+               urcrnrlon = ylon+1, urcrnrlat = 89.0,  
+               lat_0 = 90.0, lon_0 = ylon, lat_ts = 60.0)
+
+x, y = bmap(lon, lat)
+# Rescale to grid coordinates
+x = x / dx + xp
+y = y / dx + yp
+
+# Set up projection in gridmap
+gmap = gridmap.PolarStereographic(xp, yp, dx, ylon)
+x0, y0 = gmap.ll2grid(lon, lat)
+
+print "basemap: ", x, y
+print "gridmap: ", x0, y0
+print "distance [m] = ", ((x-x0)**2 + (y-y0)**2)**0.5 * dx
+
+# ------------------------
+# WGS84 case
+# ------------------------
+
+print "\n --- WGS84 --- \n"
+
+# WGS84 parameters for basemap
+a = 6378137.0
+f = 1./298.257223563
+b = a*(1-f)
+
+# Projection in basemap
+bmap = Basemap(projection='stere', rsphere=(a, b),
             llcrnrlon = ylon, llcrnrlat = 90.0,
             urcrnrlon = ylon+1, urcrnrlat = 89.0,  
             lat_0 = 90.0, lon_0 = ylon, lat_ts = 60.0)
 
-# Compute grid coordinates for the lon, lat pairs
-for lon, lat in lonlats:
-    x, y = p(lon, lat)
-    x = x / dx + xp
-    y = y / dx + yp
-    print "%9.4f %9.4f" % (x, y)
+x, y = bmap(lon, lat)
+x = x / dx + xp
+y = y / dx + yp
 
-# --------
-# WGS84
-# --------
+gmap = gridmap.PolarStereographic(xp, yp, dx, ylon, ellipsoid='WGS84')
+x0, y0 = gmap.ll2grid(lon, lat)
 
-print " --- WGS84 --- "
-
-# WGS84 parameters
-a = 6378137.0           # Major semiaxis [m]
-f = 1./298.257223563    # Flatening
-b = a*(1-f)             # Minor semiaxis [m]
-
-p = Basemap(projection='stere', rsphere=(a, b),
-            llcrnrlon = ylon, llcrnrlat = 90.0,
-            urcrnrlon = ylon+1, urcrnrlat = 89.0,  
-            lat_0 = 90.0, lon_0 = ylon, lat_ts = 60.0)
-
-for lon, lat in lonlats:
-    x, y = p(lon, lat)
-    x = x / dx + xp
-    y = y / dx + yp
-    print "%9.4f %9.4f" % (x, y)
-
-
-
-
-
-
+print "basemap: ", x, y
+print "gridmap: ", x0, y0
+print "distance [m] = ", ((x-x0)**2 + (y-y0)**2)**0.5 * dx
 
