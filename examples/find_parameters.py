fbbc2d0682a3c6f96e7127da43420b9e4044df96
Bjørn Ådlandsvik <bjorn@imr.no>
2012-08-08 09:50:38 +0200

    improved find_parameters.py

--- examples/find_parameters.py
+++ examples/find_parameters.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 """
 Reverse engingeer a polar stereographic grid to find the grid mapping
@@ -8,120 +8,142 @@ and true scale at 60 degrees north
 
 """
 
+import sys
 import numpy as np
 from netCDF4 import Dataset
 import gridmap
 
-gridfile = "demo10km_grid.nc"
 
 rad = np.pi / 180.0
 deg = 180.0 / np.pi
 
-#xp0 = 0.0
-#yp0 = 0.0
-#Lm0 = 1
-#Mm0 = 1
 
-f = Dataset(gridfile)
+def find_parameters(f):
+    """
+    Find the polar stereographic parameters from a ROMS netcdf file
 
-# The number of grid cells
-Lm = len(f.dimensions['xi_rho'])-2
-Mm = len(f.dimensions['eta_rho'])-2
-print "Lm, Mm =", Lm, Mm
+    Input:
+      f: a netCDF4.Dataset from a grid file
+
+    Returns a PolarStereographic instance
+
+    The variables lon_rho, lat_rho, angle, and pm are required
+    in the file.
+
+    Return some nonsence if the grid is not polar stereographic
+    with center at North pole, spherical earth radius 6371 km
+    and true length at 60 degrees. The function is_polstereo can be
+    used to detect this.
+    """
+     
+    lat_ts = 60.0  # Latitude of true scale
+
+    # The number of grid cells
+    Lm = len(f.dimensions['xi_rho'])-2
+    Mm = len(f.dimensions['eta_rho'])-2
+    #print "Lm, Mm =", Lm, Mm
+
+    try:
+        lon0 = f.variables['lon_rho'][0,0]
+        lat0 = f.variables['lat_rho'][0,0]
+        angle0 = f.variables['angle'][0,0]
+        pm0 = f.variables['pm'][0,0]
+    except KeyError as e:
+        v = "Missing essential variable %s in %s" % (e, ROMSfile)
+        raise KeyError(v)
+        sys.exit(1)
+
+    ylon = lon0 + angle0*deg
+    ylon = round(ylon, 2)
+    dx = 1.0/pm0 * (1 + np.sin(lat_ts*rad))/(1 + np.sin(lat0*rad))
+    dx = round(dx, 2)
+
+    # Make a grid mapping with origin at north pole
+    gmap0 = gridmap.PolarStereographic(0.0, 0.0, dx, ylon, 1, 1)
+    x, y = gmap0.ll2grid(lon0, lat0)
+    xp = round(-x, 3)
+    yp = round(-y, 3)
+
+    return gridmap.PolarStereographic(xp, yp, dx, ylon, Lm, Mm)
+
+# --------------------------------------------
+
+def is_polstereo(f, gmap, i, j):
+    """
+    Control the grid mapping of a file
+
+    Input:       
+      f : a netCDF4.Dataset from a grid file
+      gmap : a PolarSterographic grid mapping
+      i, j : indices of grid cell to test
+
+    Output:
+      True/False depending on the success
+    """
+    good = True   # no wrong tests yet
+
+    # Read some variables at the grid cell
+    lon0 = f.variables['lon_rho'][j, i]
+    lat0 = f.variables['lat_rho'][j, i]
+    angle0 = f.variables['angle'][j, i]
+
+    # Improve tests, find reasonable tolerances
+    tol = 1.0e-6
+    lon, lat = gmap.grid2ll(i, j)
+    good = good and (abs(lon-lon0) < tol) and (abs(lat-lat0) < tol)
+
+    good = good and (abs(gmap.angle(i,j)-angle0) < tol)
+
+    return good
+
+# ----------------------------------------------
+
+if __name__ == '__main__':
+
+    import sys
+    
+    #usage = "Usage: python find_parameters ROMSfile"
+    usage = "Usage: python %s ROMSfile" % sys.argv[0]
+
+    try:
+        ROMSfile = sys.argv[1]
+    except IndexError:
+        print usage
+        sys.exit(1)
+
+    try:
+        f = Dataset(ROMSfile)
+    except RuntimeError as e:
+        print "%s : %s" % (ROMSfile, e)
+        sys.exit(1)
+ 
+    # Find the parameteres
+    try:
+        gmap = find_parameters(f)
+    except KeyError as e:
+        print "KeyError:", e
+        sys.exit(1)
+        
+    i = gmap.Lm // 2
+    j = gmap.Mm // 3
+
+    good = is_polstereo(f, gmap, j, i)
+    
+    if not good:
+        print "%s : not polar stereographic grid mapping" % ROMSfile
+        sys.exit(1)
+
+    print "%s : polar stereographic grid mapping" % ROMSfile
+    print "xp   = ", gmap.xp
+    print "yp   = ", gmap.yp
+    print "dx   = ", gmap.dx
+    print "ylon = ", gmap.ylon
+    print "Lm   = ", gmap.Lm
+    print "Mm   = ", gmap.Mm
 
 
-# lon_rho
-lon0 = f.variables['lon_rho'][0,0]
-angle0 = f.variables['angle'][0,0]
-ylon = lon0 + angle0*deg
-print "ylon = ", ylon
 
-# dx
-pm0 = f.variables['pm'][0,0]
-lat0 = f.variables['lat_rho'][0,0]
-dx0 = 1.0/pm0
-dx = dx0 * (1 + np.sin(60*rad))/(1 + np.sin(lat0*rad))
-# Burde kanskje runde av lite grann
-print "dx = ", dx
+        
+        
 
-
-
-
-
-# Make a grid with origin at north pole
-gmap0 = gridmap.PolarStereographic(0.0, 0.0, dx, ylon, 1, 1)
-
-
-#lon = f0.variables['lon_rho'][:,:]
-#lat = f0.variables['lat_rho'][:,:]
-#angle0 = f0.variables['angle'][0,0]
-
-
-# gmap0 coordinates of origo
-x, y = gmap0.ll2grid(lon0, lat0)
-
-xp, yp = -x, -y
-
-print "xp, yp = ", xp, yp
-
-gmap = gridmap.PolarStereographic(xp, yp, dx, ylon, Lm, Mm)
-
-# --------
-# Control
-# --------
-
-# OK, origin in place
-i, j = 0, 0
-lo, la = gmap.grid2ll(j, i)
-print "--- i,j = ", i, j
-print "reconstructed grid : ", lo, la
-print "in file            : ", lon[j,i], lat[j,i]
-
-i, j = 70, 50  # OK, this fits
-lo, la = gmap.grid2ll(i, j)
-print "--- i,j = ", i, j
-print "reconstructed grid : ", lo, la
-print "in file            : ", lon[j,i], lat[j,i]
-
-# This is OK, meaning the value of ylon is OK
-print "--- angle at origin [degrees]"
-print "reconstructured grid : ", ylon - lon[0,0]
-print "from file            : ", angle0 * 180.0/np.pi
-
-# --------------------------
-# Generate a new grid file
-# --------------------------
-
-# Commented out, enough to do once
-#grid_name = "norkyst_800m"
-#gridmap.create_grid(gmap, grid_name)
-
-f = Dataset("norkyst_800m_grid.nc", 'a')
-
-# -----------
-# More tests
-# -----------
-
-i,j = 70, 50
-
-# Should be almost equal
-print "pm    : ", f0.variables['pm'][j,i], f.variables['pm'][j,i]
-
-# Should be equal
-print "lat_u : ", f0.variables['lat_u'][j,i], f.variables['lat_u'][j,i]
-
-# -------------------------------
-# Transfer topography and masks to new grid file
-# -------------------------------
-
-H = f0.variables['h'][:,:]
-f.variables['hraw'][0,:,:] = H
-f.variables['h'][:,:] = H
-
-f.variables['mask_rho'][:,:] = f0.variables['mask_rho'][:,:]
-f.variables['mask_u'][:,:] = f0.variables['mask_u'][:,:]
-f.variables['mask_v'][:,:] = f0.variables['mask_v'][:,:]
-f.variables['mask_psi'][:,:] = f0.variables['mask_psi'][:,:]
-
-f.close()
-
+    
